44372df82db2ea6594832d46bfef9e5a735ec36b
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-03 10:22:16 +0530

    Add files via upload

---
+++ arithematic.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class arithematic{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("c="+(a+b));
+        System.out.println("d="+(a-b));
+        System.out.println("e="+(a*b));
+        System.out.println("f="+(a/b));
+    }
+}

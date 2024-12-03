2e3b2b86183f3869156cc567c61b9a63539dda8d
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-03 14:15:58 +0530

    Add files via upload

---
+++ swappingmulti.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class swappingmulti{
+    public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+a=a*b;
+b=a/b;
+a=a/b;
+String res=String.format("after swapping\n a=%d \n b=%d",a,b);
+System.out.println(res);
+    }
+}
